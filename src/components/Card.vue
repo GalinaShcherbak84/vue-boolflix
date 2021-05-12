6364773ee048b2e77f613183eee0b9c64c6937b0
GalinaSherbak84 <galinasherbak@gmail.com>
2021-05-12 16:26:01 +0200

    stelle

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,47 +15,27 @@
             <p>Lingua originale:</p>
             <img src="@/assets/it.png" alt="">
         </div>
-      <div class="stelle" v-show="info.vote_average==0">
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
+        <div class="stelle" v-show="info.vote_average==0">
+            <i class="far fa-star" v-for="(n,index) in 5" :key ="index + 'a'" ></i>
         </div>
         <div class="stelle" v-show="Math.ceil(info.vote_average)<2 && Math.ceil(info.vote_average)>0">
             <i class="fas fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
+            <i class="far fa-star" v-for="(n,index) in 4" :key ="index + 'b'"></i>
         </div>
         <div class="stelle" v-show="Math.ceil(info.vote_average)<=4 && Math.ceil(info.vote_average)>2">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
+            <i class="fas fa-star" v-for="(n,index) in 2" :key ="index + 'c'"></i>
+            <i class="far fa-star" v-for="(n,index) in 3" :key ="index + 'd'"></i>
         </div>
         <div class="stelle" v-show="Math.ceil(info.vote_average)<=6 && Math.ceil(info.vote_average)>4 ">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
+            <i class="fas fa-star" v-for="(n,index) in 3" :key ="index + 'e'"></i>
+            <i class="far fa-star" v-for="(n,index) in 2" :key ="index + 'f'"></i>
         </div>
         <div class="stelle" v-show="Math.ceil(info.vote_average)<=8 && Math.ceil(info.vote_average)>6">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
+            <i class="fas fa-star" v-for="(n,index) in 4" :key ="index + 'g'"></i>
             <i class="far fa-star"></i>
         </div>
         <div class="stelle" v-show="Math.ceil(info.vote_average)<=10 && Math.ceil(info.vote_average)>8">
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
-            <i class="fas fa-star"></i>
+            <i class="fas fa-star" v-for="(n,index) in 5" :key ="index + 'h'"></i>
         </div> 
     </div>
 </template>
